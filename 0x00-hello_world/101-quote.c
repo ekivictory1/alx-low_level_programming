819b25ccc36136f48217659f47ef330856681da7
Eki Victory <106748977+ekivictory1@users.noreply.github.com>
2022-06-30 09:03:14 +0100

    0x00 hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
 return (1);
 }
