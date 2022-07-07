f087e3ba601f5109a3954974dae9e591d4b2d7a3
Eki Victory <106748977+ekivictory1@users.noreply.github.com>
2022-07-07 09:14:55 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,42 +6,43 @@
  */
 void print_times_table(int n)
 {
-	int a, rep, b;
+	int a = 0, rep, b;
 
-	if (n <= 0 && n >= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			for (a = 0; a <= n; a++)
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
 			{
-				rep = a * b;
-				if (b == 0)
-				{
-					_putchar(rep + '0');
-				} else if (rep < 10 && a != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(rep + '0');
-				} else if (rep < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((rep / 10) + '0');
-					_putchar((rep % 10) + '0');
-				} else if (rep >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((rep / 100) + '0');
-					_putchar(((rep / 10) % 10) + '0');
-					_putchar((rep % 10) + '0');
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
 			}
-			_putchar('\n');
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		a++;
 	}
 }
